4cead84cf142a4caf22c8fdbc9b67bb992ec1729
pnisarg <pnisarg26@yahoo.com>
2015-11-10 14:59:37 -0800

    Right now tab partially completed

---
+++ index.php
@@ -0,0 +1,123 @@
+<!--
+Author: Nisarg Patel
+Date: 11/08/2015
+Home Work Assignment 8 CSCI 571
+-->
+<?php
+$usStatesList=array(""=>"Select your state... ","AL"=>"Alabama","AK"=>"Alaska","AZ"=>"Arizona","AR"=>"Arkansas","CA"=>"California","CO"=>"Colorado","CT"=>"Connecticut","DE"=>"Delaware","FL"=>"Florida","GA"=>"Georgia","HI"=>"Hawaii","ID"=>"Idaho","IL"=>"Illinois","IN"=>"Indiana","IA"=>"Iowa","KS"=>"Kansas","KY"=>"Kentucky","LA"=>"Louisiana","ME"=>"Maine","MD"=>"Maryland","MA"=>"Massachusetts","MI"=>"Michigan","MN"=>"Minnesota","MS"=>"Mississippi","MO"=>"Missouri","MT"=>"Montana","NE"=>"Nebraska","NV"=>"Nevada","NH"=>"NewHampshire","NJ"=>"NewJersey","NM"=>"NewMexico","NY"=>"NewYork","NC"=>"NorthCarolina","ND"=>"NorthDakota","OH"=>"Ohio","OK"=>"Oklahoma","OR"=>"Oregon","PA"=>"Pennsylvania","RI"=>"RhodeIsland","SC"=>"SouthCarolina","SD"=>"SouthDakota","TN"=>"Tennessee","TX"=>"Texas","UT"=>"Utah","VT"=>"Vermont","VA"=>"Virginia","WA"=>"Washington","WV"=>"WestVirginia","WI"=>"Wisconsin","WY"=>"Wyoming");
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Forecast</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/forecast.css">
+        <script src="./js/jquery-2.1.4.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/forecast.js"></script>
+        <script src="./js/jquery.validate.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">Forecast Search</div>
+            <div class="row">
+                <form id="forecastForm" method="GET" action="./forecast.php/getForecast">
+                    <fieldset>
+                        <div class="col-md-3 form-group nopadding">
+                            <label class="control-label" for="streetAddress">Street Address:<span class="star">*</span></label>
+                            <input type="text" class="form-control"  name="streetAddress" id="streetAddress"  placeholder="Enter Street Address">
+                        </div>
+                        <div class="col-md-2 form-group nopadding">
+                            <label class="control-label" for="city">City:<span class="star">*</span></label>
+                            <input type="text" class="form-control" name="city" id="city" placeholder="Enter the city name">
+                        </div>
+                        <div class="col-md-2 form-group">
+                            <label class="control-label" for="state">State:<span class="star">*</span></label>
+                            <select class="form-control" name="state" id="state">
+                                <?php
+                                foreach($usStatesList as $key => $value ){
+                                    echo "<option value='$key' >$value</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="col-md-6 form-group" style="padding: 0px;">
+                                <label class="control-label" for="Degree">Degree:<span class="star">*</span></label><br>
+                                <input type="radio" class="form-group" name="degree" value="us" checked> Fahrenheit </input><span> &nbsp;</span>
+                            <input type="radio" class="form-group" name="degree" value="si"> Celsius  </input>
+                        </div>
+
+                    <div class="col-md-6 form-group mainFormButtons" align="right" style="padding: 0px;"><br>
+                        <button type="submit" name="submit" id="submitButton" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Submit
+                        </button>
+                        <button type="button" class="btn btn-default" id="clearButton">
+                            <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Clear
+                        </button>
+                    </div>
+                    <div class="form-group form-inline" align="right">
+                        Powered by:
+                        <a href="http://forecast.io" alt="Forecast.io">
+                            <img src="./images/forecast_logo.png" class="form-group" alt="Forecast" height="50px" width="120px">
+                        </a>
+                    </div>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div> <!-- row end-->
+    <hr>
+    <div class="result">
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#rightNow" aria-controls="home" role="tab" data-toggle="tab">Right Now</a></li>
+            <li role="presentation"><a href="#next24Hours" aria-controls="profile" role="tab" data-toggle="tab">Next 24 hours</a></li>
+            <li role="presentation"><a href="#next7days" aria-controls="messages" role="tab" data-toggle="tab">Next 7 Days</a></li>
+        </ul>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="rightNow">
+                <div class="row">
+                    <div class="col-md-6" id="currentWeatherTable">
+
+                        <div class="col-md-6 currentWeatherTableHeader" id="weatherImage"></div>
+                        <div class="col-md-6 currentWeatherTableHeader">
+                            <ul class="temperatureDiv">
+                                <li id="summary">Mostly Cloudy in Los Angeles, CA</li>
+                                <li >
+                                    <span id="temperature">72</span>
+                                    <span id="unit">&deg; F</span>
+                                </li>
+                                <li id="highLow">L: 70 &deg; | H: 80 &deg;</li>
+                            </ul>
+                        </div>
+                        
+                        <table class="table table-striped table-responsive">
+                            <tr><td>Precipitation</td><td>None</td></tr>
+                            <tr class="danger"><td>Chance of Rain</td><td>None</td></tr>
+                            <tr><td>Wind Speed</td><td>None</td></tr>
+                            <tr class="danger"><td>Dew Point</td><td>None</td></tr>
+                            <tr><td>Humidity</td><td>None</td></tr>
+                            <tr class="danger"><td>Visibility</td><td>None</td></tr>
+                            <tr><td>Sunrise</td><td>None</td></tr>
+                            <tr class="danger"><td>Sunset</td><td>None</td></tr>
+                        </table>
+                    </div>
+                    <div class="col-md-6" id="currentWeatherMap"></div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="next24Hours">I'm profile tab</div>
+            <div role="tabpanel" class="tab-pane" id="next7days">I'm messages tab</div>
+        </div>
+
+    </div><!-- End of class result -->
+    </div> <!-- container end-->
+</body>
+</html>
--- js/forecast.js
+++ js/forecast.js
@@ -1,37 +1,95 @@
 $('document').ready(function(){
-    
+
+//        $('.result').hide();
+
     jQuery.validator.addMethod("noSpaces", function(value, element) {
         return this.optional(element) || value.trim().length > 0;
     });    //validates input against spaces
-    
+
     //Form validation 
-  $('#forecastForm').validate({
-      rules:{
-          streetAddress: {
-              noSpaces: true,
-              required: true
-          },
-          city: {
-            noSpaces: true,
-            required: true
-          },
-          state: "required"
-      },
-      messages: {
-          streetAddress: "Please enter the street address",
-          city: "Please enter the city",
-          state: "Please select a state"
-      },
-      submitHandler: function(form){
-          console.log("submit " + form);
-      }
-  });
-    
+    $('#forecastForm').validate({
+        rules:{
+            streetAddress: {
+                noSpaces: true,
+                required: true
+            },
+            city: {
+                noSpaces: true,
+                required: true
+            },
+            state: "required"
+        },
+        messages: {
+            streetAddress: "Please enter the street address",
+            city: "Please enter the city",
+            state: "Please select a state"
+        },
+        submitHandler: function(form){
+            $.ajax({
+                url: form.action,
+                type: form.method,
+                data: $(form).serialize(),
+                success: successFunction,
+                error: function(){
+                    alert("Failed to submit form!");
+                }
+            });
+        }
+    });
+
     //clear form
     $('#clearButton').click(function(){
         var form = $('#forecastForm');
         form.validate().resetForm();
         form[0].reset();
+        $('.result').hide();
     });
 });
+
+//Array for icon value mapping
+var iconValue = {
+    "clear-day" : "clear.png",
+    "clear-night" : "clear_night.png",
+    "rain" : "rain.png",
+    "snow" : "snow.png",
+    "sleet" : "sleet.png",
+    "wind" : "wind.png",
+    "fog" : "fog.png",
+    "cloudy" : "cloudy.png",
+    "partly-cloudy-day" : "cloud_day.png",
+    "partly-cloudy-night" : "cloud_night.png"
+};
+
+function setImage(image){
+    var imageUrl = "./images/" + iconValue[image];
+    var img = '<img id="weatherIcon" src="'+imageUrl+'">';
+    $('#weatherImage').html(img);
+}
+
+function setTemperature(result){
+    
+}
+//Generate content for rightNow Tab 
+function generateRightNow(result){
+    setImage(result.currently.icon);
+    setTemperature(result);
+}
+//Generate content for Next 24 Hours Tab 
+function generateNext24(result){
+    
+}
+//Generate content for Next 7 Days Tab 
+function generateNext7(result){
+    
+}
+//populate Data
+function successFunction(response){
+    var result = JSON.parse(response);
+    if(result != null){
+        $('.result').show();
+        generateRightNow(result);
+        generateNext24(result);
+        generateNext7(result);
+    }
+}
 
